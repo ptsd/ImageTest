a81171bdbe3824e217b2cf79cdbf0599f3f60e8d
Patrick Dobbs <ptsd@patrickdobbs.co.uk>
2021-07-09 02:19:11 +0100

    Initial

---
+++ build.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const { hashElement } = require('folder-hash');
+
+const options = {
+  files: { exclude: ['.gitignore'] },
+};
+
+console.log('Creating a hash over the current folder:');
+hashElement('./build/images', options)
+  .then(hash => {
+    console.log(hash.toString());
+    try {
+      const data = fs.writeFileSync('./build/resources.json', hash.toString());
+      //file written successfully
+    } catch (err) {
+      console.error(err);
+    }
+  })
+  .catch(error => {
+    return console.error('hashing failed:', error);
+  });
